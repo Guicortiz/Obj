e10ec05648fb7642fb071e9d32d409386aace677
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-04 12:52:10 -0300

    Herançã

--- Program.cs
+++ Program.cs
@@ -6,20 +6,37 @@ namespace Obj
     {
         static void Main(string[] args)
         {
-            var payment = new Payment();
+            var payment = new PaymentCard();
+            payment.VerifyFoundsinCard();
+            payment.Finaldate = DateTime.Now;
+            payment.CreditcardNumber = 12345678;
+
+            var paymentdoc = new PaymentDoc();
+            paymentdoc.Document = "12345678";
+            paymentdoc.DocumentNumber();
 
         }
 
         class Payment
         {
-            DateTime Finaldate;
+            public DateTime Finaldate;
 
             public void pay()
             {
-                VerifyFoundsinCard();
-            }
 
-            private void VerifyFoundsinCard() { }
+            }
+        }
+
+        class PaymentCard : Payment
+        {
+            public int CreditcardNumber;
+            public void VerifyFoundsinCard() { }
+        }
+
+        class PaymentDoc : Payment
+        {
+            public string Document;
+            public void DocumentNumber() { }
         }
     }
 }
