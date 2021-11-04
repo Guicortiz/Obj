1089a595a4cf434412d32a912dbed6f3304c6174
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-04 12:32:54 -0300

    Classe e objeto

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Obj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Customer = new Customer();
+            Customer.Name = "Guicortiz";
+            Console.WriteLine(Customer.Name);
+        }
+
+        // Objeto(classe) é um tipo de referencia.
+        // Struct é um tipo de valor.
+        class Customer
+        {
+            public string Name;
         }
     }
 }
