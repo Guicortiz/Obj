70474e901ac66afc576e7201983a96821ed5b37b
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-04 19:46:19 -0300

    Propriedades

--- Program.cs
+++ Program.cs
@@ -7,11 +7,35 @@ namespace Obj
         static void Main(string[] args)
         {
             var payment = new Payment();
+            payment.description = "New payment";
+            payment.Value = 10;
+            Console.WriteLine(payment.Value);
+
+
         }
         //private, protected, internal e public
         class Payment
         {
+            //prop, propg e propfull
             DateTime Finaldate;
+
+            public string description { get; set; }
+
+            private Decimal _Value;
+            public Decimal Value
+            {
+                get
+                {
+                    _Value = _Value * 2;
+                    return _Value;
+                }
+                set
+                {
+                    _Value = _Value - 2;
+                    _Value = value;
+                }
+            }
+
 
             Address Billingaddress;
 
