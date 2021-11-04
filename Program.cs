d4a79865e4fe53c648fceb724d9d714460670c7e
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-04 19:35:44 -0300

    Tipos complexo

--- Program.cs
+++ Program.cs
@@ -7,28 +7,19 @@ namespace Obj
         static void Main(string[] args)
         {
             var payment = new Payment();
-            payment.Finaldate = DateTime.Now;
-            payment.pay();
-
         }
         //private, protected, internal e public
         class Payment
         {
-            public DateTime Finaldate;
+            DateTime Finaldate;
 
-            protected void pay()
-            {
+            Address Billingaddress;
 
-            }
         }
 
-        class PaymentDocument : Payment
+        public class Address
         {
-            void payment()
-            {
-                base.pay();
-
-            }
+            string zipCode;
         }
     }
 }
