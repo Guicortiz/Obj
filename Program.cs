80e642e6f37ac92b9ca1b886c95f3f0d34d6d831
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-08 20:48:43 -0300

    List

--- Program.cs
+++ Program.cs
@@ -1,32 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Obj
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            var subscription = new Subscription();
-            var context = new DataContext<Person>();
-            var context2 = new DataContext<Payment>();
-            context.Save(person);
-            context.Save(subscription);
-        }
+            //var payments = new IEnumerable<Payment>();
+            //var payments = new ICollection<Payment>();
+            //var payments = new List<Payment>();
 
+            IList<Payment> payments = new List<Payment>();
+            payments.Add(new Payment(1));
+            payments.Add(new Payment(2));
+            payments.Add(new Payment(3));
+            payments.Add(new Payment(4));
+            payments.Add(new Payment(5));
+
+            foreach (var item in payments)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+            var payment = payments.Where(x => x.Id == 3);
+            var payment2 = payments.First(x => x.Id == 3);
+            payments.Remove(payment2);
+
+            foreach (var item in payments)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+            var paidPayment = new List<Payment>();
+            paidPayment.AddRange(payments);
+
+            payments.AsEnumerable();
+            payments.ToList();
+            payments.ToArray();
+
+        }
     }
 
-    public class DataContext<T>
-    where T : Person
+    public class Payment
     {
-        public void Save(T entity)
+        public Payment(int id)
         {
-
+            Id = id;
         }
+
+        public int Id { get; set; }
     }
-
-    public class Person { }
-
-    public class Payment { }
-
-    public class Subscription { }
 }
